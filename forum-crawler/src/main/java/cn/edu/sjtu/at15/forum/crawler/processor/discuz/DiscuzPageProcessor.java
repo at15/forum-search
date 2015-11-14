bbab085ccfa8459799582518b9f644cc38f58009
at15 <at15@outlook.com>
2015-11-14 22:00:35 +0800

    Add logic for processor
    determine type of page and chose corresponding action

--- forum-crawler/src/main/java/cn/edu/sjtu/at15/forum/crawler/processor/discuz/DiscuzPageProcessor.java
+++ forum-crawler/src/main/java/cn/edu/sjtu/at15/forum/crawler/processor/discuz/DiscuzPageProcessor.java
@@ -27,15 +27,27 @@ public class DiscuzPageProcessor implements PageProcessor {
     }
 
     public void process(Page page) {
-        LOGGER.debug("processing page : " + page.getUrl().toString());
+        String currentUrl = page.getUrl().toString();
+        LOGGER.debug("processing page : " + currentUrl);
+        if (!discuzUrl.isInner(currentUrl)) {
+            LOGGER.debug("not inner link : " + currentUrl);
+            return;
+        }
         // determine the type of this page
-        // TODO
-        // 1. if this is not inner link, ignore it.
-        // 2. if this is a list page, get all the links in this page, and add other pages
+        if (discuzUrl.isThread(currentUrl)) {
+            // TODO: parse thread content here
+            // in fact we should handle all the comments as well, but this is not so important
+            return;
+        }
+        // TODO: 2. if this is a list page, get all the links in this page, and add other pages
         // NOTE: discuzz will show the total page in the last of pagination
-        // 3. if this is a thread page, abstract the info we need
-        for (String l : page.getHtml().links().all()) {
-            LOGGER.debug(l);
+        if (discuzUrl.isList(currentUrl)) {
+            // TODO: get all the threads in the list
+            // we don't consider nested list.
+            for (String l : page.getHtml().links().all()) {
+                LOGGER.debug(l);
+            }
+            return;
         }
     }
 
