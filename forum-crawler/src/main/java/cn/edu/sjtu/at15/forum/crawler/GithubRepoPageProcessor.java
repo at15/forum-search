7d76e758971e4a7a25c512f074733d3cb316fe2d
at15 <at15@outlook.com>
2015-11-14 16:40:09 +0800

    Fix #4
    thanks to hsqlu https://github.com/code4craft/webmagic/issues/226

--- forum-crawler/src/main/java/cn/edu/sjtu/at15/forum/crawler/GithubRepoPageProcessor.java
+++ forum-crawler/src/main/java/cn/edu/sjtu/at15/forum/crawler/GithubRepoPageProcessor.java
@@ -3,6 +3,7 @@ package cn.edu.sjtu.at15.forum.crawler;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.ConsolePipeline;
 import us.codecraft.webmagic.pipeline.JsonFilePipeline;
 import us.codecraft.webmagic.processor.PageProcessor;
 
@@ -15,7 +16,8 @@ public class GithubRepoPageProcessor implements PageProcessor {
     public void process(Page page) {
         page.addTargetRequests(page.getHtml().links().regex("(https://github\\.com/\\w+/\\w+)").all());
         page.putField("author", page.getUrl().regex("https://github\\.com/(\\w+)/.*").toString());
-        page.putField("name", page.getHtml().xpath("//h1[@class='entry-title public']/strong/a/text()").toString());
+        // 官方文档里的xpath抽取不出来内容, 所以导致抓不出结果, name一直是null, 所以就都被skip了
+        page.putField("name", page.getHtml().xpath("//h1[contains(@class, 'entry-title') and contains(@class, 'public')]/strong/a/text()").toString());
         if (page.getResultItems().get("name") == null) {
             //skip this page
             page.setSkip(true);
@@ -30,7 +32,8 @@ public class GithubRepoPageProcessor implements PageProcessor {
     public static void main(String[] args) {
         Spider.create(new GithubRepoPageProcessor())
                 .addUrl("https://github.com/code4craft")
-                .addPipeline(new JsonFilePipeline("/Users/gpl/repos/github.com/at15/forum-search/data"))
+//                .addPipeline(new JsonFilePipeline("/Users/gpl/repos/github.com/at15/forum-search/data"))
+                .addPipeline(new ConsolePipeline())
                 .thread(5)
                 .run();
     }
