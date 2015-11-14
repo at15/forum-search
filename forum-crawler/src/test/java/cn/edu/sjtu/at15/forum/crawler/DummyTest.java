c06a395e642815318226b6c7dd3708a150196d57
at15 <at15@outlook.com>
2015-11-14 13:36:19 +0800

    Add test and travis

---
+++ forum-crawler/src/test/java/cn/edu/sjtu/at15/forum/crawler/DummyTest.java
@@ -0,0 +1,14 @@
+package cn.edu.sjtu.at15.forum.crawler;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by gpl on 15/11/14.
+ */
+public class DummyTest {
+    @Test
+    public void testDummy() {
+        Assert.assertEquals("dummy", "dummy");
+    }
+}
