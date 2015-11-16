44af6945edd9a59361327ca4111ab6f9d7ccc924
at15 <at15@outlook.com>
2015-11-17 02:23:46 +0800

    Add config for use yarn
    - we can see the mapreduce resource manager on 8088
    - but all job fails due to bin/java is not found, and can't use hadoop commandline to upload file to hdfs ... strange

---
+++ scripts/README.md
@@ -0,0 +1,6 @@
+# FAQ
+
+- Q: Mapreduce fail after switched to yarn and got error logs like 
+
+ A: see https://cloudcelebrity.wordpress.com/2014/01/31/yarn-job-problem-application-application_-failed-1-times-due-to-am-container-for-xx-exited-with-exitcode-127/
+    need to config bin/java .... but wtf why it can't use JAVA_HOME ?
--- scripts/start_hadoop.sh
+++ scripts/start_hadoop.sh
@@ -4,6 +4,10 @@ cd ../bundle
 echo "copy the configs"
 cp config/core-site.xml hadoop-2.7.1/etc/hadoop
 cp config/hdfs-site.xml hadoop-2.7.1/etc/hadoop
+# for yarn
+cp config/mapred-site.xml hadoop-2.7.1/etc/hadoop
+cp config/yarn-site.xml hadoop-2.7.1/etc/hadoop
 cp config/hadoop-env.sh hadoop-2.7.1/etc/hadoop
 echo "start hadoop as daemon"
 hadoop-2.7.1/sbin/start-dfs.sh
+hadoop-2.7.1/sbin/start-yarn.sh
