6bcd69c45d01a0d8f0ba22853ee6a6ca6f2f98e7
at15 <at15@outlook.com>
2015-11-17 00:38:31 +0800

    Add jackson
    and use it to write crawed data

---
+++ forum-crawler/src/main/java/cn/edu/sjtu/at15/forum/crawler/processor/discuz/DiscuzJsonFilePipline.java
@@ -0,0 +1,35 @@
+package cn.edu.sjtu.at15.forum.crawler.processor.discuz;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import us.codecraft.webmagic.ResultItems;
+import us.codecraft.webmagic.Task;
+import us.codecraft.webmagic.pipeline.Pipeline;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by gpl on 15/11/16.
+ */
+public class DiscuzJsonFilePipline implements Pipeline {
+    private final static Logger LOGGER = LoggerFactory.getLogger(DiscuzJsonFilePipline.class);
+    private final String baseFolder;
+    private final static ObjectMapper mapper = new ObjectMapper();
+
+    public DiscuzJsonFilePipline(String baseFolder) {
+        this.baseFolder = baseFolder;
+    }
+
+    public void process(ResultItems resultItems, Task task) {
+        DiscuzThread thread = resultItems.get("thread");
+        // TODO: thread logic here
+
+    }
+
+    public void write(DiscuzThread thread) throws IOException {
+        LOGGER.debug("current directory " + System.getProperty("user.dir"));
+        mapper.writeValue(new File("../data/t.json"), thread);
+    }
+}
---
+++ forum-crawler/src/test/java/cn/edu/sjtu/at15/forum/crawler/processor/discuz/DiscuzJsonFilePiplineTest.java
@@ -0,0 +1,23 @@
+package cn.edu.sjtu.at15.forum.crawler.processor.discuz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * Created by gpl on 15/11/17.
+ */
+public class DiscuzJsonFilePiplineTest {
+    @Test
+    public void testWrite() throws IOException {
+        DiscuzThread thread = new DiscuzThread();
+        thread.setTitle("xiaoming");
+        thread.setAuthor("xiaowang");
+        thread.setViewCount(100);
+        thread.setReplyCount(100);
+        thread.setAuthorPost("this is some dummy content");
+        DiscuzJsonFilePipline pipline = new DiscuzJsonFilePipline("aaa");
+        pipline.write(thread);
+    }
+}
