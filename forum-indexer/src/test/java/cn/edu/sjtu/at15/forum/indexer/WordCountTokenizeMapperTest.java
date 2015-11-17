f5939afe1878786253dad70c7c0e1e1f868d366d
at15 <at15@outlook.com>
2015-11-17 17:15:17 +0800

    Add unit test for word count mapper
    this works fine locally, but will it fail on travis?

---
+++ forum-indexer/src/test/java/cn/edu/sjtu/at15/forum/indexer/WordCountTokenizeMapperTest.java
@@ -0,0 +1,31 @@
+package cn.edu.sjtu.at15.forum.indexer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * Created by gpl on 15/11/17.
+ */
+public class WordCountTokenizeMapperTest {
+    MapDriver<Object, Text, Text, IntWritable> mapDriver;
+
+    @Before
+    public void setUp() {
+        WordCountTokenizerMapper mapper = new WordCountTokenizerMapper();
+        mapDriver = MapDriver.newMapDriver(mapper);
+    }
+
+    @Test
+    public void testMapper() throws IOException {
+        mapDriver.withInput(new Text("a"), new Text("a b c"));
+        mapDriver.withOutput(new Text("a"), new IntWritable(1));
+        mapDriver.withOutput(new Text("b"), new IntWritable(1));
+        mapDriver.withOutput(new Text("c"), new IntWritable(1));
+        mapDriver.runTest();
+    }
+}
