69350fb0ae478a2f0201d7350d6444dba2fb5570
at15 <at15@outlook.com>
2015-11-19 21:02:06 +0800

    Change scripts and hadoop config to make it work on ubuntu
    should have a .example and ignore the real config

--- bundle/config/hadoop-env.sh
+++ bundle/config/hadoop-env.sh
@@ -22,7 +22,7 @@
 # remote nodes.
 
 # The java implementation to use.
-export JAVA_HOME=${JAVA_HOME}
+export JAVA_HOME=/usr/lib/jvm/java-7-sun
 
 # The jsvc implementation to use. Jsvc is required to run secure datanodes
 # that bind to privileged ports to provide authentication of data transfer
