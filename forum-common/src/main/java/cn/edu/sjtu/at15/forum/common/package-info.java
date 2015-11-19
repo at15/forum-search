ac6a3633d0be24ba94b297c10b42812ada0057f4
at15 <at15@outlook.com>
2015-11-19 14:17:50 +0800

    Add forum common module

---
+++ forum-common/src/main/java/cn/edu/sjtu/at15/forum/common/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by gpl on 15/11/19.
+ */
+package cn.edu.sjtu.at15.forum.common;
--- forum-indexer/scripts/cat.sh
+++ forum-indexer/scripts/cat.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source common.sh
-${DFS} -cat /user/at15/output/9/*
+${DFS} -cat /user/at15/output/10/*
