758739a8e0b4e1ce56cae90172c90625c27e5b9c
at15 <at15@outlook.com>
2015-11-22 20:20:21 +0800

    Add mapper test for main thread and fixtures

--- forum-indexer/src/test/java/cn/edu/sjtu/at15/forum/indexer/mapreduce/ThreadMapperTest.java
+++ forum-indexer/src/test/java/cn/edu/sjtu/at15/forum/indexer/mapreduce/ThreadMapperTest.java
@@ -26,4 +26,16 @@ public class ThreadMapperTest {
 //        mapDriver.runTest();
         Assert.assertEquals("a", "a");
     }
+
+    @Test
+    public void testThreadMap() throws IOException {
+        ThreadMapper threadMapper = new ThreadMapper();
+        MapDriver<Object, Text, Text, Text> mapDriver = new MapDriver<Object, Text, Text, Text>(threadMapper);
+        String content = FileUtils.readFileAsString("fixtures/sub-thread.json");
+        mapDriver.setMapInputPath(new Path("/user/at15/sub-abc.json"));
+        mapDriver.withInput(new Text("ignore"), new Text(content));
+        mapDriver.withOutput(new Text("a"), new Text("b"));
+//        mapDriver.runTest();
+        Assert.assertEquals(1, 1);
+    }
 }
